fe468c21da46c36aee6944b843d5badaf3775fd4
Emerson Suarez <emersonsuarez2904@gmail.com>
2022-12-28 21:00:00 -0500

    Proyecto Terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactosComponent } from './pages/contactos/contactos.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
 
 const routes: Routes = [
   {path:'',component:InicioComponent}, //redireccionar al Inicio
   {path:'inicio',component:InicioComponent},//redireccionar al Inicio
   {path:'nosotros',component:NosotrosComponent},//redireccionar al Nosotros
-  { path: '**', redirectTo: '' }// cualquier url redirigir al Inicio
+  {path:'servicios',component:ServiciosComponent},//redireccionar al Servicios
+  {path:'contactos',component:ContactosComponent},//redireccionar al Contactos
+  { path: '**',pathMatch:'full',redirectTo: '' }// cualquier url redirigir al Inicio
 ];
 
 @NgModule({
